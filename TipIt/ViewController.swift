6b42b3afed902fc6dca4a33f695c9c2b2658b4f6
Angela Wen <wena@seas.upenn.edu>
2019-08-28 21:22:43 -0700

    Initial Commit

---
+++ TipIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipIt
+//
+//  Created by Angela Wen on 8/28/19.
+//  Copyright © 2019 Angela Wen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
